2534adafb81205a707932cdb4a145f24b1e470a0
Sahil Panchasara <sahilpanchasara822@gmail.com>
2022-04-14 10:46:39 +0530

    updated

--- models/file.js
+++ models/file.js
@@ -1,21 +1,14 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const fileSchema = new Schema(
-  {
-    filename: { type: String, required: true },
-    path: { type: String, required: true },
-    size: { type: Number, required: true },
-    uuid: { type: String, required: true },
-    sender: { type: String, required: false },
-    receiver: { type: String, required: false },
-    expireAt: {
-      type: Date,
-      default: Date.now,
-      expires: 86400,
-    },
-  },
-  { timestamps: true }
-);
+const fileSchema = new Schema({
+  filename: { type: String, required: true },
+  path: { type: String, required: true },
+  size: { type: Number, required: true },
+  uuid: { type: String, required: true },
+  sender: { type: String, required: false },
+  receiver: { type: String, required: false },
+  createdAt: { type: Date, expires: 86400, default: Date.now },
+});
 
 module.exports = mongoose.model("File", fileSchema);
